b7701647795909af5b6528a535e0459466704115
Johana catalina rios torres <jcriostorres@gmail.com>
2020-10-06 20:38:24 -0700

    Agrega las operaciones a la capa de presentación.

---
+++ target/classes/dataset/schema-h2.sql
@@ -0,0 +1,24 @@
+--DROP TABLE public.heroes;
+
+CREATE TABLE heroes
+(
+    id integer NOT NULL,
+    name character varying(255),
+    CONSTRAINT heroes_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO public.heroes(
+	id, name)
+	VALUES (1, 'Bat-Man');
+
+INSERT INTO public.heroes(
+	id, name)
+	VALUES (2, 'Spider-Man');
+
+INSERT INTO public.heroes(
+	id, name)
+	VALUES (3, 'Hulk');
+
+INSERT INTO public.heroes(
+	id, name)
+	VALUES (2, 'Superman');
